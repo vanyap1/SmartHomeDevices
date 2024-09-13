68e68d1531d95f949f113861ff2eefd63e345de4
Ivan <princ.va@gmail.com>
2024-09-13 12:29:24 +0300

    freeze fixed

--- py_udp/main.py
+++ py_udp/main.py
@@ -25,12 +25,12 @@ class MainThread():
 
     def run(self): 
         while True:
-            #dataArray = ExternalHelper.build_byte_array()
+            dataArray = ExternalHelper.build_byte_array()
             
-            dataArray = ExternalHelper.buildMessage("Python3") 
+            #dataArray = ExternalHelper.buildMessage("Python3") 
             send_byte_array(dataArray, HOST_BROADCAST, TX_PORT)
             print(dataArray)
-            time.sleep(30)
+            time.sleep(5)
 
 
 
